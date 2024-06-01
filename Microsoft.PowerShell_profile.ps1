b30c6df7b2879d5d59ead1eb51ca968ac5535348
ahunter1289 <ashunter27@gmail.com>
2024-06-01 02:22:18 -0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,7 +6,7 @@ New-Alias py "$userProfile\AppData\Local\Programs\Python\Python39\python.exe"
 New-Alias python "$userProfile\AppData\Local\Programs\Python\Python39\python.exe"
 New-Alias python3 "$userProfile\AppData\Local\Programs\Python\Python39\python.exe"
 
-New-Alias activateocpinb "$userProfile\Documents\code\python_envs\ocpi_win39_ocpinb_dev_loose\win39ocpinbdev\Scripts\Activate.ps1"
+New-Alias activateocpinb "$userProfile\Documents\code\python_envs\win39ocpinbdev\Scripts\Activate.ps1"
 
 function openps {
     code "$userProfile\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
